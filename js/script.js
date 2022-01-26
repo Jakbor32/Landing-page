31dbe0d40772b53cd8f1be8cb29c4267be7b279b
Jakub Borowy <borowyjakub32@gmail.com>
2022-01-26 18:25:18 +0100

    file update

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Slider
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,4000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+// Slider - opacity onscroll
+window.onscroll = function() {
+    var slides = document.getElementById("slides");
+    slides.style.opacity =  (1.3 - window.pageYOffset / 700);
+    console.log(window.pageYOffset);
+     
+}
+   
+   
