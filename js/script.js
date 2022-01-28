07956136df23ce37cb670b781739fd700e5ebbb9
Jakub Borowy <borowyjakub32@gmail.com>
2022-01-28 20:45:03 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Slider
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,4000);
+var slideInterval = setInterval(nextSlide,6000);
 
 function nextSlide() {
     slides[currentSlide].className = 'slide';
